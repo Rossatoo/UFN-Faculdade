1f16d1d49239b5b6ff3720b95621ae4ee47a006f
Matheus Rossato <102099800+Rossatoo@users.noreply.github.com>
2024-03-25 19:10:39 -0300

    Delete CRUD directory

--- CRUD/src/main/java/com/atividade1/Repository/DepartamentoRepository.java
+++
@@ -1,7 +0,0 @@
-package com.atividade1.Repository;
-
-import com.atividade1.Model.Departamento;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface DepartamentoRepository extends JpaRepository<Departamento, Integer> {
-}
--- CRUD/src/main/java/com/atividade1/Repository/DependenteRepository.java
+++
@@ -1,7 +0,0 @@
-package com.atividade1.Repository;
-
-import com.atividade1.Model.Dependente;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface DependenteRepository extends JpaRepository<Dependente, Integer> {
-}
