323424c9cfa2b93f839386e082dfa746037a97e9
Matheus Rossato <102099800+Rossatoo@users.noreply.github.com>
2024-03-27 14:53:27 -0300

    Delete ProgramacaoWEB/CRUD directory

--- ProgramacaoWEB/CRUD/src/main/java/com/atividade1/Repository/FuncionarioRepository.java
+++
@@ -1,7 +0,0 @@
-package com.atividade1.Repository;
-
-import com.atividade1.Model.Funcionario;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface FuncionarioRepository extends JpaRepository<Funcionario, Integer> {
-}
